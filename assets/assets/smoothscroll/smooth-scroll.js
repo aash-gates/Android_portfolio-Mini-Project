3c5d0bf848e623093ae21593cb5f53f04728655a
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-08-29 12:14:32 +0530

    Update smooth-scroll.js

--- assets/assets/smoothscroll/smooth-scroll.js
+++ assets/assets/smoothscroll/smooth-scroll.js
@@ -86,6 +86,7 @@
 
         var body = document.body;
         var html = document.documentElement;
+
     if (typeof define === 'function' && define.amd)
         define(function() {
             return SmoothScroll;
