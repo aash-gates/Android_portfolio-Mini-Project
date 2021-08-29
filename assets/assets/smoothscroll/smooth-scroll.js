8d6cad845a125afd98d573573ee235779ed23abc
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-08-29 12:12:47 +0530

    Update smooth-scroll.js

--- assets/assets/smoothscroll/smooth-scroll.js
+++ assets/assets/smoothscroll/smooth-scroll.js
@@ -60,4 +60,5 @@
 
     var key = { left: 37, up: 38, right: 39, down: 40, spacebar: 32,
         pageup: 33, pagedown: 34, end: 35, home: 36 };
+    var arrowKeys = { 37: 1, 38: 1, 39: 1, 40: 1 };
 
