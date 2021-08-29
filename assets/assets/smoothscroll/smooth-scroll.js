3daede8c28ec64e14a9f21a88bf4a81570c73f29
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-08-29 12:18:06 +0530

    Update smooth-scroll.js

--- assets/assets/smoothscroll/smooth-scroll.js
+++ assets/assets/smoothscroll/smooth-scroll.js
@@ -86,6 +86,7 @@
 
         var body = document.body;
         var html = document.documentElement;
+        window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
             get: function () {
                 supportsPassive = true;
             }
