cb58d6d3e1b966703f264513549686c10cee5629
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-08-29 12:19:43 +0530

    Update smooth-scroll.js

--- assets/assets/smoothscroll/smooth-scroll.js
+++ assets/assets/smoothscroll/smooth-scroll.js
@@ -86,6 +86,7 @@
 
         var body = document.body;
         var html = document.documentElement;
+        if (x >= 1) return 1;
         if (x <= 0) return 0;
 
         if (options.pulseNormalize == 1) {
