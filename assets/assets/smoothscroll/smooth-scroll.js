518d468ea2286f1691ab54ca16725fc34346839f
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-08-29 12:15:12 +0530

    Update smooth-scroll.js

--- assets/assets/smoothscroll/smooth-scroll.js
+++ assets/assets/smoothscroll/smooth-scroll.js
@@ -87,6 +87,7 @@
         var body = document.body;
         var html = document.documentElement;
 
+
     /***********************************************
      * PUBLIC INTERFACE
      ***********************************************/
