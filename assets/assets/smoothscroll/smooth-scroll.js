ae6229ef9bca8e9dfc3dcd6a8a87ba4b654227d5
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-08-29 12:14:40 +0530

    Update smooth-scroll.js

--- assets/assets/smoothscroll/smooth-scroll.js
+++ assets/assets/smoothscroll/smooth-scroll.js
@@ -86,6 +86,7 @@
 
         var body = document.body;
         var html = document.documentElement;
+
     if (window.SmoothScrollOptions) // async API
         SmoothScroll(window.SmoothScrollOptions);
 
